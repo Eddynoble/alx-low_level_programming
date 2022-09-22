6ddefebf4412910846c617b02215f623e595a58a
Edet Ekpo <edet.ekpo001@gmail.com>
2022-09-22 15:28:12 -0700

    nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,44 +1,30 @@
 #include <stdio.h>
 
 /**
-*main - program that prints either number
-*or fizz or buzz or fizzBuzz
+* main - program that prints either number or ..
+* @void: No parameter
 *
-*
-*Return: returns 0
+* Return: Return 0 on success
 */
-
 int main(void)
 {
-	int num = 1;
+	int num;
 
 	while (num++ < 100)
-	{
+
 		if ((num % 3 == 0) && (num % 5 == 0))
-		{
 			printf("FizzBuzz ");
-		}
 		else if ((num % 3) == 0)
-		{
 			printf("Fizz ");
-		}
 		else if ((num % 5) == 0)
 		{
 			if (num != 100)
-			{
 				printf("Buzz ");
-			}
 			else
-			{
 				printf("Buzz");
-			}
 		}
 		else
-		{
 			printf("%d ", num);
-		}
-	}
 	printf("\n");
-
 	return (0);
 }
