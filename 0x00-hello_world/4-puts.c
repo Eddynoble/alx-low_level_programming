b9b5e5774b989ff7aff1bd8bd332720b9b701b8e
Edet Ekpo <edet.ekpo001@gmail.com>
2022-09-22 11:28:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- *main - print the string and put it in the function
+ * main - print the string and put in the function
  *
- *Description: Use the main function
- *prints exactly "programming is like building a multilingual puzzle"
- *Return: 0
+ * Decription: Use the main function
+ * Prints exactly "Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
-       	puts("\"programming is like building a multilingual puzzle");
-       	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
