c53998288fabc88ea5267b06ebbdff3f45e1f5d7
Edet Ekpo <edet.ekpo001@gmail.com>
2022-09-22 10:56:33 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 /**
- * main - Positive anything is better than negative nothing 
+ * main - Positive anything is better than negative nothing
  *
  * Description: To print alphabet
  * Return: 0
