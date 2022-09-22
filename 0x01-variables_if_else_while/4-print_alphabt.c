38c0a9ffe934cf0fd525405717ffa59a6753fbda
Edet Ekpo <edet.ekpo001@gmail.com>
2022-09-22 10:52:51 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Positive anything is better than negative nothing 
+ *
+ * Description: To print alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
